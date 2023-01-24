464386befab922d900c02af83221a05810148aec
Xizheng (Hugh) Yu <52917220+x12hengyu@users.noreply.github.com>
2023-01-24 15:54:05 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-# install docker, adapted from https://docs.docker.com/engine/install/ubuntu/
-sudo apt-get update
+if [[ $(which docker) && $(docker --version) ]]; then
+    echo "Docker is installed.\n"
+  else
+    echo "Install docker.\n"
+    # install docker, adapted from https://docs.docker.com/engine/install/ubuntu/
+    sudo apt-get update
 
-sudo apt-get install \
-  ca-certificates \
-  curl \
-  gnupg \
-  lsb-release
-  
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo apt-get install \
+      ca-certificates \
+      curl \
+      gnupg \
+      lsb-release
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  
-# install docker engine
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-sudo apt-get update
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo docker run hello-world
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    # install docker engine
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    sudo apt-get update
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    sudo docker run hello-world
+fi
+
